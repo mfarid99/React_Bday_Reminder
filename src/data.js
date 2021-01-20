4fa41f509d23eefc29f273012ff44dc44dad7c78
moe <moe.faridd@gmail.com>
2021-01-19 23:31:17 -0500

    final commit

--- src/data.js
+++ src/data.js
@@ -1,37 +1,47 @@
 export default [
   {
     id: 1,
-    name: 'Bertie Yates',
-    age: 29,
+    name: 'Alien 3',
+    year: 1992,
+    starring: "Sigourney Weaver, Charles S. Dutton",
+    boxoffice: "$159.8 million USD",
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+      'https://static.wikia.nocookie.net/alienfilmspedia/images/d/d0/Alien-3-poster.jpg/revision/latest/scale-to-width-down/340?cb=20111210093142',
   },
   {
     id: 2,
-    name: 'Hester Hogan',
-    age: 32,
+    name: 'Seven',
+    year: 1995,
+    starring: "Brad Pitt, Morgan Freeman",
+    boxoffice: "$327.3 million USD",
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+      'https://images-na.ssl-images-amazon.com/images/I/51VQ890AJML._SY445_.jpg',
   },
   {
     id: 3,
-    name: 'Larry Little',
-    age: 36,
+    name: 'Fight Club',
+    year: 1999,
+    starring: "Brad Pitt, Edward Norton",
+    boxoffice: "$101.2 million USD",
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+      'https://m.media-amazon.com/images/M/MV5BMmEzNTkxYjQtZTc0MC00YTVjLTg5ZTEtZWMwOWVlYzY0NWIwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY1200_CR85,0,630,1200_AL_.jpg',
   },
   {
     id: 4,
-    name: 'Sean Walsh',
-    age: 34,
+    name: 'Panic Room',
+    year: 2002,
+    starring: "Jodie Foster, Kristen Stewart",
+    boxoffice: "$197.1 million USD",
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+      'https://m.media-amazon.com/images/M/MV5BNmY4ZGFjYTYtMDNmYi00ZDM4LWFjYTgtNmNlZjBmMzg0MzQ3XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_.jpg',
   },
   {
     id: 5,
-    name: 'Lola Gardner',
-    age: 29,
+    name: 'Seven',
+    year: 2014,
+    starring: "Ben Affleck, Rosamund Pike",
+    boxoffice: "$369.3 million USD",    
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+      'https://m.media-amazon.com/images/M/MV5BMTk0MDQ3MzAzOV5BMl5BanBnXkFtZTgwNzU1NzE3MjE@._V1_.jpg',
   },
 ];
